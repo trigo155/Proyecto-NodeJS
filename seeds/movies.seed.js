e349169984365025f60c94b185af50a8837a8f4e
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-03 21:59:22 +0200

    Add new movie

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -3,16 +3,28 @@ const Movie = require('../models/Movie.model');
 const db = require('../db');
 
 const movies = [{
-    title: 'Con la muerte en los talones',
-    year: 1959,
-    director: 'Alfred Hitchcock',
-    actors: 'Cary Grant, Eva Marie Saint, James Mason, Martin Landau',
-    country: 'United States, California',
-    duration: '2h 16m',
-    summary: 'Roger O.Thornhill es confundido por George Kaplan, un agente del gobierno. Consigue escapar aunque lo siguen de cerca',
-    filmProducer: '',
-    gender: 'Suspense',
-}];
+        title: 'Con la muerte en los talones',
+        year: 1959,
+        director: 'Alfred Hitchcock',
+        actors: 'Cary Grant, Eva Marie Saint, James Mason, Martin Landau',
+        country: 'United States, California',
+        duration: '2h 16m',
+        summary: 'Roger O.Thornhill es confundido por George Kaplan, un agente del gobierno. Consigue escapar aunque lo siguen de cerca.',
+        filmProducer: '',
+        gender: 'Suspense',
+    },
+    {
+        title: 'Con faldas y a loco',
+        year: 1959,
+        director: 'Alfred Hitchcock',
+        actors: 'Marilyn Morroe, Jack Lemmon, Joe E. Brown, George Raft',
+        country: 'United States, Coronado, California',
+        duration: '2h 12m',
+        summary: 'Dos testigos de asesinatos se disfrazan de miembros de una banda de mujeres para confundir a la mafia que les persigue.',
+        filmProducer: '',
+        gender: 'Comedy',
+    }
+];
 
 mongoose
     .connect(db.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
