d814e380cbc715276a99398f2d7d687bea780a52
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-03 22:38:44 +0200

    Modify routes of index

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const db = require('./db');
+const moviesRoutes = require('./routes/Movie.routes')
 
 const PORT = 3000;
 const app = express();
 const router = express.Router();
-
-app.use('/', router);
 db.connect();
 
-app.listen(PORT, () => {
-    console.log(`Servidor funicionando en http://localhost:${PORT}`);
-});
+app.use('/', router);
+app.use('/movies', moviesRoutes)
 
 router.get('/', (req, res) => {
     const msg = 'Proyecto de NodeJS';
     res.send(msg);
 })
+
+app.listen(PORT, () => {
+    console.log(`Servidor funicionando en http://localhost:${PORT}`);
+});
