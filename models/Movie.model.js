6a7d8e2a2de4ff2df791cf70201391543b1180a7
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-03 20:41:22 +0200

    Add Movie model

---
+++ models/Movie.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const MovieSchema = new Schema({
+        title: { type: String, required: true },
+        year: { type: Number, required: true },
+        director: { type: String, required: true },
+        actors: { type: String, required: true },
+        country: { type: String },
+        duration: { type: String },
+        summary: { type: String },
+        filmProducer: { type: String },
+        gender: {
+            type: String,
+            required: true,
+            enum: ['Comedy, Drama, Action, Animation, Musical, Romance, Terror, Sci-fi, Suspense']
+        },
+
+    }, { timestamps: true }
+
+);
+
+const Movie = mongoose.model('Movies', MovieSchema);
+
+module.exports = Movie;
