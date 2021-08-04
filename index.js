e5530850b1f2d328acebea1041837b0a69578ca3
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-04 06:05:09 +0200

    Add file view/movies.hbs

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const app = express();
 const router = express.Router();
 db.connect();
 
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use('/', router);
-app.use('/movies', moviesRoutes)
+app.use('/movies', moviesRoutes);
+// app.use('*', (req, res, next) => {
+//     const err = new Error('No encontramos la ruta que solicita, disculpe');
+//     return res.status(404).render(err.message)
+
+// });
 
 router.get('/', (req, res) => {
     const msg = 'Proyecto de NodeJS';
