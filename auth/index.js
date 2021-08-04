764723b0665c1bf44435bf4ab1642d65a45bda82
Juan José Trigo Hormigos <trigo.hormigos@bootcamp-upgrade.com>
2021-08-04 06:55:16 +0200

    Add to auth index.js

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,27 @@
 const passport = require('passport');
+const registerStrategy = require('./register.strategy');
+const loginStrategy = require('./login.strategy');
+
+
+const User = require('../models/User.model');
+
+passport.serializeUser((user, done) => {
+    return done(null, user._id);
+});
+
+passport.deserializeUser(async(userId, done) => {
+    try {
+        const existInUser = await User.findById(userId);
+        return done(null, existInUser);
+
+    } catch (err) {
+        return done(err);
+    }
+});
+
+const setStrategies = () => {
+    passport.use('registro', registerStrategy);
+    passport.use('regisgro', loginStrategy);
+};
+
+module.exports = { setStrategies };
